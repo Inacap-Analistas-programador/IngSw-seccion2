48c7ab65807e7239df1920435a19c3d60125130b
Ezequiel <ezequiel.ferreira@inacapmail.cl>
2025-10-22 23:29:01 -0300

    conexion de todas las pantallas al router y app.vue

--- SistemScoutsClient/src/router/index.js
+++ SistemScoutsClient/src/router/index.js
@@ -8,17 +8,21 @@ const PagosView = () => import('@/views/PagosView.vue')
 const Correos = () => import('@/views/Correos.vue')
 const ManualAcreditation = () => import('@/views/ManualAcreditation.vue')
 const VerificadorQR = () => import('@/views/VerificadorQR.vue')
+const Usuarios = () => import('@/views/Gestionpersonas.vue')
+const CursosCapacitaciones = () => import('@/views/CRUDcursos.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'dashboard', component: Dashboard },
+    { path: '/usuarios', name: 'usuarios', component: Usuarios },
     { path: '/mantenedores', name: 'mantenedores', component: Mantenedores },
     { path: '/gestionpersonas', name: 'gestionpersonas', component: Gestionpersonas },
     { path: '/pagos', name: 'pagosview', component: PagosView },
   { path: '/manual-acreditacion', name: 'manualacreditacion', component: ManualAcreditation },
   { path: '/verificador-qr', name: 'verificadorqr', component: VerificadorQR },
     { path: '/correos', name: 'correos', component: Correos },
+    { path: '/cursos-capacitaciones', name: 'cursoscapacitaciones', component: CursosCapacitaciones },
     // fallback
     { path: '/:catchAll(.*)', redirect: '/' },
   ],
