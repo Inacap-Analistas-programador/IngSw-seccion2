57fc5e22da63d07dd1819002b248452cb5ac81ff
Ezequiel <ezequiel.ferreira@inacapmail.cl>
2025-10-30 01:00:42 -0300

    fix error Vieww Api

--- SystemScoutsApi/ApiCoreScouts/Routers/Cursos_router.py
+++ SystemScoutsApi/ApiCoreScouts/Routers/Cursos_router.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from ..Views.Cursos_view import *
+from ..View.Cursos_view import *
 
 router = DefaultRouter()
 
