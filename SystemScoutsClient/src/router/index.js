48cd06cde3c60922844eb49757515203d0ff26e5
Ezequiel <ezequiel.ferreira@inacapmail.cl>
2025-10-30 01:07:07 -0300

    Arreglando error de view Reporte

--- SystemScoutsClient/src/router/index.js
+++ SystemScoutsClient/src/router/index.js
@@ -12,7 +12,6 @@ const ManualAcreditation = () => import('@/views/ManualAcreditation.vue')
 const VerificadorQR = () => import('@/views/VerificadorQR.vue')
 const UsuariosRoles = () => import('@/views/UsuariosRoles.vue')
 const CursosCapacitaciones = () => import('@/views/CRUDcursos.vue')
-const Reportes = () => import('@/views/Reportes.vue')
 const Login = () => import('@/views/Login.vue')
 const FormularioPreInscripcion = () => import('@/views/Formulario.vue')
 const FormularioPreInscripcion2 = () => import('@/views/Formulario 2.vue')
@@ -32,7 +31,6 @@ const router = createRouter({
   { path: '/verificador-qr', name: 'verificadorqr', component: VerificadorQR, meta: { requiresAuth: true, requiresAuth: true } },
     { path: '/correos', name: 'correos', component: Correos, meta: { requiresAuth: true } },
     { path: '/cursos-capacitaciones', name: 'cursoscapacitaciones', component: CursosCapacitaciones, meta: { requiresAuth: true } },
-    { path: '/reportes', name: 'reportes', component: Reportes, meta: { requiresAuth: true } },
     { path: '/inscripciones', name: 'formularioPreInscripcion', component: FormularioPreInscripcion },
   { path: '/inscripciones-2', name: 'formularioPreInscripcion2', component: FormularioPreInscripcion2 },
     // fallback
