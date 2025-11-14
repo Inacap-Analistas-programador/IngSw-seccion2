21de81f720f9581f1bf2603ad21b84631bbfb0a4
Ezequiel <ezequiel.ferreira@inacapmail.cl>
2025-11-14 01:46:33 -0300

    produccion Frontend Vue

--- SystemScoutsClient/vite.config.js
+++ SystemScoutsClient/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/IngSw-seccion2/',   // 👈 OBLIGATORIO PARA GITHUB PAGES
   plugins: [
     vue(),
     vueDevTools(),
@@ -16,3 +17,4 @@ export default defineConfig({
     },
   },
 })
+
